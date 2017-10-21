50edd270f1e975c12fae8285396010ff89705b37
pedroalexxxferreira <pedroalexxxferreira@gmail.com>
2017-10-21 17:23:33 +0100

    YAY degree hist works!

--- tests2.py
+++ tests2.py
@@ -1,11 +1,18 @@
 from network import Network
-
+import matplotlib.pyplot as plt
+import numpy as np
 net = Network()
 
-net.ER_Random(100,0.2)
+net.ER_Random(1000,0.2)
 net.ShowNodes()
 
 for n in range(net.NodeCount()):
     print(str(n)+str((net.InDegree(n),net.OutDegree(n))))
 
 print(net.ConditionalDegreeDistribution())
+
+
+#x = np.array([0,2,4,4,5,6])
+#y = np.array([1,2,1,7,8,5])
+#plt.hist2d(x,y)
+plt.show()
