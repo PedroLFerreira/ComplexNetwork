7ddfdab4318fc1fb17bea89ab856ef4f140ac4db
Antonio <acapela96@hotmail.com>
2017-10-22 15:29:20 +0100

    BetweennessCentrality

--- tests.py
+++ tests.py
@@ -7,10 +7,17 @@ net.ER_Random(V=20, p=0.1)
 #net.Init([[1,0], [2,0], [3,0], [4,0], [5,0], [0,1], [0,2], [0,3], [0,4], [0,5]])
 net.ShowNodes()
 net.DrawNetwork()
-print(net.Neiborghs(0))
 print(net.AvClusteringCoefficient())
 
 print(net.ClosenessCentrality(0))
 print(net.HarmonicCentrality(0))
-print(net.BetweennessCentrality())
-print(net.EigenvectorCentrality())
+bd1 = net.BetweennessCentrality()
+bd2 = net.BetweennessCentrality2()
+EC  = net.EigenvectorCentrality()
+
+print("compare algorthms:")
+print("\t BC1        BC2        EC")
+for key in net.nodes:
+    print(key, "{:8f}   {:8f}   {:8f}".format(bd1[key], bd2[key], EC[key]), sep='\t')
+
+#print(net.EigenvectorCentrality())
