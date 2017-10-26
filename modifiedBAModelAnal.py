57db7019497b2522dc2311d01fd60ccd9633145b
Antonio <acapela96@hotmail.com>
2017-10-26 14:38:41 +0100

    Clustering coefficient

--- modifiedBAModelAnal.py
+++ modifiedBAModelAnal.py
@@ -14,36 +14,55 @@ def distribution(n, k):
 
 net = Network()
 
-c = 2
+m=2
+n=500
 
-n=100
-net.ModifiedBA_Random22(N=n, k=c, alpha=0)#, initialNetwork = [[0,1], [1,2], [2,0]])
-ddist = net.DegreeDistribution(loglogscale = False, cum = True, showPlot = False)
+CC = []
+CCerror = []
+x = np.linspace(0, 1, 10)
+for alpha in x:
+    temp = []
+    for iteration in range(5):
+        net.ModifiedBA_Random22(n, m, alpha=alpha)
+        ddist = net.DegreeDistribution(showPlot=False, cum=True)
+        temp.append(net.AvClusteringCoefficient())
+    
+    CC.append(sum(temp)/len(temp))
+    CCerror.append(np.std(temp)/len(temp))
 
-x = np.arange(0, len(ddist), dtype=float)
-x = x[c:]
-y = np.array(ddist[c:])
-
-popt, pcov = curve_fit(powerLaw, x, y, p0=(3,1))
-print(popt)
-
-fig = plt.figure()
-ax = fig.add_subplot(111)
-plt.xscale('log')
-plt.yscale('log')
-plt.scatter(x,y)
-plt.plot(x,powerLaw(x,*popt),'r--')
-plt.ylabel('comulative probability')
-plt.xlabel('k')
-
-ax.text(50, .1, "$\\gamma={:.3f}$\n$a={:.3f}$".format(popt[0], popt[1]), fontsize=15)
-
-plt.savefig('BApowerlog.pdf')
+plt.errorbar(x, CC, yerr=CCerror)
 plt.show()
 
-print(net.AvClusteringCoefficient())
+# c = 2
 
-net.DrawNetwork(useForce=True)
+# n=100
+# net.ModifiedBA_Random22(N=n, k=c, alpha=0.5)#, initialNetwork = [[0,1], [1,2], [2,0]])
+# ddist = net.DegreeDistribution(loglogscale = False, cum = True, showPlot = False)
+
+# x = np.arange(0, len(ddist), dtype=float)
+# x = x[c:]
+# y = np.array(ddist[c:])
+
+# popt, pcov = curve_fit(powerLaw, x, y, p0=(3,1))
+# print(popt)
+
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# plt.xscale('log')
+# plt.yscale('log')
+# plt.scatter(x,y)
+# plt.plot(x,powerLaw(x,*popt),'r--')
+# plt.ylabel('comulative probability')
+# plt.xlabel('k')
+
+# ax.text(50, .1, "$\\gamma={:.3f}$\n$a={:.3f}$".format(popt[0], popt[1]), fontsize=15)
+
+# plt.savefig('BApowerlog.pdf')
+# plt.show()
+
+# print(net.AvClusteringCoefficient())
+
+# net.DrawNetwork(useForce=True)
 
 # b = net.BetweennessCentrality()
 # d = dict(((key, net.Degree(key)) for key in net.nodes))
